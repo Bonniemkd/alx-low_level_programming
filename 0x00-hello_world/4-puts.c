8a89f803004d7acdfda294173aed17a61488dc09
Bonniemkd <bonniemkd@gmail.com>
2023-07-14 00:24:53 -0700

    pust

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a progra  that prints a line with  puts funtion
+ * Return: the program should return 0 
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
