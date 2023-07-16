9a13cd7ebd7c5a2a79b44885112b6a6f98aa92d4
Bonniemkd <bonniemkd@gmail.com>
2023-07-16 01:58:53 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * main -  C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line
+ * main -  C program that prints exactly with p
+ * roper grammar, but the outcome is a piece of
+ * art,, followed by a new line
  * Return: 0 (Success)
  */
 int main(void)
